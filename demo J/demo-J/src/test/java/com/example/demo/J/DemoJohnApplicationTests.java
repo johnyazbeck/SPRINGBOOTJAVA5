34b22cb2f4b12c287600c788888f38ee39d66c32
johnyazbeck <125282806+johnyazbeck@users.noreply.github.com>
2025-09-18 22:04:31 +0200

    saving basic hello world
    1st save

---
+++ demo J/demo-J/src/test/java/com/example/demo/J/DemoJohnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJohnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
